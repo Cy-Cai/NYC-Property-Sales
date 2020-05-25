889a1857a2139fc56434fc4c1e65408b2c19cedf
Cy-Cai <cyruschoi10@gmail.com>
2020-05-25 11:34:42 -0400

    Complete the model 3: adding the high pricing area as a factor. The improvement is siginificant!

--- Modeling.R
+++ Modeling.R
@@ -1,10 +1,12 @@
 if(!require(broom)) install.packages("broom", repos = "http://cran.us.r-project.org")
 if(!require(graphics)) install.packages("graphics", repos = "http://cran.us.r-project.org")
 if(!require(UsingR)) install.packages("UsingR", repos = "http://cran.us.r-project.org")
+if(!require(forcats)) install.packages("forcats", repos = "http://cran.us.r-project.org")
+
 library(broom)
 library(graphics)
 library(UsingR)
-
+library(forcats)
 head(train_set)
 sapply(train_set, class)
 
@@ -227,4 +229,131 @@ MillionProperty %>% filter(BOROUGH=="3") %>%
   group_by(BUILDING.CLASS.AT.TIME.OF.SALE) %>% 
   summarise(n=NROW(SALE.PRICE),m=mean(SALE.PRICE)) %>% 
   arrange(desc(m)) %>% filter(n>10,m>1000000)
+
+
+# price per square feet varies in Manhattan. What is the driver of the differece?
+train_set %>% mutate(price_per_square_feet=SALE.PRICE/GROSS.SQUARE.FEET) %>% 
+  ggplot(aes(x=factor(BOROUGH),y=price_per_square_feet))+
+  geom_boxplot(outlier.colour="black", outlier.shape=16,
+               outlier.size=2, notch=FALSE)
+
+train_set %>% mutate(price_per_square_feet=SALE.PRICE/GROSS.SQUARE.FEET) %>% 
+  ggplot(aes(x=factor(BUILDING.CLASS.AT.TIME.OF.SALE),y=price_per_square_feet))+
+  geom_boxplot(outlier.colour="black", outlier.shape=16,
+               outlier.size=2, notch=FALSE)
+
+
+train_set %>% mutate(price_per_square_feet=SALE.PRICE/GROSS.SQUARE.FEET) %>% 
+  ggplot(aes(x=fct_reorder(NEIGHBORHOOD,price_per_square_feet,.desc=TRUE),y=price_per_square_feet))+
+  geom_boxplot(outlier.colour="black", outlier.shape=16,
+               outlier.size=2, notch=FALSE)
+
+train_set %>% mutate(price_per_square_feet=SALE.PRICE/GROSS.SQUARE.FEET) %>% 
+  group_by(ZIP.CODE) %>% 
+  summarise(m=mean(price_per_square_feet),s=sd(price_per_square_feet),n=NROW(Prop_ID)) %>% 
+  arrange(desc(m))
+
+train_set %>% mutate(price_per_square_feet=SALE.PRICE/GROSS.SQUARE.FEET) %>% 
+  ggplot(aes(x=fct_reorder(ZIP.CODE,price_per_square_feet,.desc=TRUE),y=price_per_square_feet))+
+  geom_boxplot(outlier.colour="black", outlier.shape=16,
+               outlier.size=2, notch=FALSE)
+
+HighPricingArea<- train_set %>% mutate(price_per_square_feet=SALE.PRICE/GROSS.SQUARE.FEET) %>% 
+  group_by(ZIP.CODE) %>% 
+  summarise(m=mean(price_per_square_feet),n=NROW(Prop_ID)) %>% 
+  arrange(desc(m)) %>% 
+  mutate(PPSF=case_when(m>550~1,
+                                        TRUE ~ 0))
+sum(x[x$m>550,]$n)/sum(x$n)
+
+#MOdel 3:  SALE.PRICE~GROSS.SQUARE.FEET+ IS.HIGH.PRICING.AREA
+#y=b0+b1*x1+b2*(I(MANHATTAN))+b3*(I(HIGH.PRICING.AREA))
+
+
+#adding a column: if it is in Manhattan, it equals to Gross Square Feet; otherwise 0
+#adding a column: if it is in Manhattan, it equals to Gross Square Feet; otherwise 0
+
+temp <- train_set %>% 
+                   left_join(HighPricingArea,by=c("ZIP.CODE")) %>% 
+  mutate(GSFandManhattan=case_when(BOROUGH=="1"~ 1,
+                                   TRUE ~ 0     )*GROSS.SQUARE.FEET,
+         GSFandHIGH.PRICING.AREA=PPSF*GROSS.SQUARE.FEET)
+  
+
+
+fit_IsManhattanAndHighPricingArea<- lm(SALE.PRICE~GROSS.SQUARE.FEET+GSFandManhattan+ GSFandHIGH.PRICING.AREA,data = temp)
+
+tidy(fit_IsManhattanAndHighPricingArea)
+summary(fit_IsManhattanAndHighPricingArea)    
+
+#save the model coefficient for the predcition calculation
+b0<-fit_IsManhattanAndHighPricingArea$coefficients[1]
+b1<-fit_IsManhattanAndHighPricingArea$coefficients[2]
+b2<-fit_IsManhattanAndHighPricingArea$coefficients[3]
+b3<-fit_IsManhattanAndHighPricingArea$coefficients[4]
+
+#Performance
+newd<-test_set %>% 
+  left_join(HighPricingArea,by=c("ZIP.CODE")) %>% 
+  mutate(GSFandManhattan=case_when(BOROUGH=="1"~ 1,
+                                   TRUE ~ 0     )*GROSS.SQUARE.FEET,
+         GSFandHIGH.PRICING.AREA=PPSF*GROSS.SQUARE.FEET) %>%  
+  dplyr::select(GROSS.SQUARE.FEET,GSFandManhattan,GSFandHIGH.PRICING.AREA) 
+
+
+predicted_price<- predict(fit_IsManhattanAndHighPricingArea,newd,interval="prediction")
+rmse_IsManhattanAndHighPricingArea_model<-RMSE(test_set$SALE.PRICE,predicted_price[,1])  
+## aberrorper_simplist_model<-AbEorrPer(test_set$SALE.PRICE,predicted_price[,1])
+
+#Observe the RMSE by Property ID
+#Create temp table to draw plot; calculate the residual by property 
+temp_p<-test_set %>%
+  left_join(HighPricingArea,by=c("ZIP.CODE")) %>% 
+  mutate(GSFandManhattan=case_when(BOROUGH=="1"~ 1,
+                                   TRUE ~ 0     )*GROSS.SQUARE.FEET,
+         GSFandHIGH.PRICING.AREA=PPSF*GROSS.SQUARE.FEET) %>%  
+  mutate(predicted_price=b0+b1*GROSS.SQUARE.FEET+b2*GSFandManhattan+b3*GSFandHIGH.PRICING.AREA) %>% 
+  mutate(residul=(SALE.PRICE-predicted_price)^2) %>%
+  mutate(r=sqrt(residul))
+
+MED<-median(temp_p$r)
+
+#Store the results 
+rmse_results <-rbind(rmse_results, data.frame(method="Is Manhattan and High Pricing Area",RMSE_in_thousand=round(rmse_IsManhattanAndHighPricingArea_model/1000,0),MED_in_thousand=round(median(temp_p$r)/1000,0))%>%
+                       mutate(k=round(RMSE_in_thousand/MED_in_thousand,0),Adj_r_sq=summary(fit_IsManhattanAndHighPricingArea)[9]))
+
+#Histogram
+temp_p%>% filter(r<4*MED)%>%  ggplot(aes(r))+geom_histogram(bins=20)+
+  labs(x="Residual",y="Property Count",title="Histogram Property Count vs Residual")+
+  geom_vline(xintercept=median(temp_p$r))
+
+#Observation: there are some prediction outliners; long tail 
+
+
+#Create a for loop to create bar chart for each variable
+
+names(train_set)[c(2,3,4,5,9,12,15,18,19,20,21)]
+loop.vector<-c(2,3,4,5,9,12,15,18,19,20,24)
+# library(UsingR)
+# par(mfrow=c(3,ceiling(length(loop.vector)/3)))
+# par(mfrow=c(1,2))
+
+loop.names<-names(temp_p)
+len<-ncol(temp_p)
+for (i in loop.vector) {
+  #Observe the RMSE by Borough
+  print(i)
+  plot<-temp_p%>%dplyr::select(i,len-1) %>% 
+    rename(A=1) %>%
+    group_by(A) %>%
+    summarise(r=sqrt(mean(residul)),n=NROW(A))%>%
+    ggplot(aes(A,r,fill=n))+geom_bar(stat = "identity") +
+    labs(x=loop.names[i],y="Residual")+scale_fill_gradient(low="blue", high="red")+
+    geom_text(aes(label=round(r,3)),size=3,check_overlap = TRUE,position=position_dodge(width=0.9), vjust=-0.25)+theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+    geom_hline(yintercept=rmse_IsManhattan_model)
+  
+  print(plot)
+}
+
+
 
